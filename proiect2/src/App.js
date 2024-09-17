1493bf248ef49e85c655ee254ef8042f7f3f3501
Teo <teodoramomoc9@gmail.com>
2024-09-17 12:37:15 +0300

    create app

---
+++ proiect2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react'
+
+class App extends ReadableByteStreamController.Component{
+  constructor(){
+    super();
+    this.state = {}
+  }
+  render(){
+    return(
+      <div>
+        11curs 5
+      </div>
+    )
+  }
+    
+  
+}
+
+export default App;
